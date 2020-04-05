34116b7476479cc6d3c3c6befc2c63e6e1ffcee1
Joaquin Felber <jfelberzeta@gmail.com>
2020-04-05 02:58:22 +0200

    Output sorted and extended.  getting team match data consistently, data unified by match date as Key

--- clean_data.py
+++ clean_data.py
@@ -1,5 +1,7 @@
 import pickle
+from datetime import datetime
 
+import numpy as np
 import pandas as pd
 
 
@@ -12,7 +14,7 @@ def getParticipantsId(match, summonerName):
 def getParticipantById(participants, id):
     for p in participants:
         if p["participantId"] == id:
-            return p["stats"]
+            return p
 
 
 teamPlayers = ["Sopapiglobo", "Darkonia", "Scσrpiσn", "Blackéyé", "Flokii", "Strucio"]
@@ -21,32 +23,115 @@ teamPlayers = ["Sopapiglobo", "Darkonia", "Scσrpiσn", "Blackéyé", "Flokii", 
 data = pickle.load(open("data/filteredMatches.pickle", "rb"))
 df = pd.DataFrame.from_dict(data)
 
-# prepare list with playerStats
+
+def getMyTeam(match, teamPlayers):
+
+    player = match.participantIdentities[0]["player"]
+    if player["summonerName"] in teamPlayers:
+        return 100
+    else:
+        return 200
+
+
+# team stats
+teamStats = {}
+for _index, match in df.iterrows():
+    date = match["gameCreation"]
+    date = datetime.fromtimestamp(date / 1000)
+    myTeam = int(getMyTeam(match, teamPlayers) / 100 - 1)
+    matchStats = match.teams[myTeam]
+    matchStats
+    print(pd.DataFrame.from_dict(matchStats))
+    teamStats[date] = pd.DataFrame.from_dict(matchStats, orient="index").T
+
+
+teamStats = pd.concat(teamStats)
+teamStats.to_csv("output/team/teamStats.csv")
+
+
+# prepare list with playerStats and playerTimeline
 playerStats = {}
+playerTimelines = {}
 for player in teamPlayers:
-    playerStats[player] = []
+    playerStats[player] = {}
+    playerTimelines[player] = {}
 
 # assign match data to players
 for _index, match in df.iterrows():
+    date = match["gameCreation"]
+    date = datetime.fromtimestamp(date / 1000)
     for player in teamPlayers:
         id = getParticipantsId(match, player)
         if id is not None:
-            type(getParticipantById(match["participants"], id))
-            stats = getParticipantById(match["participants"], id)
-            playerStats[player].append(stats)
+            p = getParticipantById(match["participants"], id)
+            playerStats[player][date] = p["stats"]
+            playerTimelines[player][date] = p["timeline"]
+
 
 # save allStats
-
+quickReviews = {}
 for player in teamPlayers:
 
-    playerStats[player] = pd.DataFrame.from_dict(playerStats[player])
+    # stats
+    playerStats[player] = pd.DataFrame.from_dict(playerStats[player]).T
     playerStats[player]["counter"] = 1
-    playerStats[player].to_csv("output/allStats_" + player + ".csv")
+    playerStats[player].to_csv("output/raw/allStats_" + player + ".csv")
+
+    # timelines
+    playerTimelines[player] = pd.DataFrame.from_dict(
+        playerTimelines[player], orient="index"
+    )
+    playerTimelines[player]["counter"] = 1
+
+    # unpack dictionaries
+    cols = [
+        "creepsPerMinDeltas",
+        "xpPerMinDeltas",
+        "goldPerMinDeltas",
+        "damageTakenPerMinDeltas",
+        "csDiffPerMinDeltas",
+        "xpDiffPerMinDeltas",
+        "damageTakenDiffPerMinDeltas",
+    ]
+    time_windows = ["0-10", "10-20", "20-30", "30-end"]
+
+    # create placeholders
+    for col in cols:
+        for win in time_windows:
+            type(playerTimelines[player][col])
+            playerTimelines[player][col + "@" + win] = playerTimelines[player][col]
+
+    # fill placeholders by unpacking dicts
+    for col in cols:
+        for win in time_windows:
+            for index, _row in playerTimelines[player].iterrows():
+                try:
+                    playerTimelines[player].at[
+                        index, col + "@" + win
+                    ] = playerTimelines[player][col][index][win]
+                except (KeyError, TypeError):
+                    playerTimelines[player].at[index, col + "@" + win] = np.nan
+
+    playerTimelines[player].to_csv("output/raw/timelines_" + player + ".csv")
 
     # drop_cols = [c for c in playerStats[player].columns if c.lower()[:4]
     #     in ['perk', 'stat', 'play', 'item', 'tota', 'larg', ']
     # drop_cols.extend(['participantId'])
-    playerStats[player][
+
+    # quickReviews
+    pt_select = playerTimelines[player][
+        [
+            "creepsPerMinDeltas@0-10",
+            "creepsPerMinDeltas@10-20",
+            "xpPerMinDeltas@0-10",
+            "csDiffPerMinDeltas@0-10",
+            "csDiffPerMinDeltas@10-20",
+            "xpDiffPerMinDeltas@0-10",
+            "xpDiffPerMinDeltas@10-20",
+        ]
+    ]
+
+    ps_select = playerStats[player][
         [
             "win",
             "kills",
@@ -57,16 +142,33 @@ for player in teamPlayers:
             "goldEarned",
             "visionScore",
         ]
-    ].to_csv("output/quickReview_" + player + ".csv")
+    ]
 
+    quickReviews[player] = pd.merge(
+        ps_select, pt_select, left_index=True, right_index=True
+    )
+    quickReviews[player].astype(float).round(2).to_csv(
+        "output/players/quickReview_" + player + ".csv"
+    )
 
+playerStats["Strucio"]
 # create team summary
 teamSummary = []
 for player in teamPlayers:
-    means = playerStats[player].mean()
-    means = means[["win", "kills", "deaths", "assists"]]
+    means = quickReviews[player].mean()
+    means = means[
+        [
+            "win",
+            "kills",
+            "deaths",
+            "assists",
+            "creepsPerMinDeltas@10-20",
+            "csDiffPerMinDeltas@0-10",
+            "csDiffPerMinDeltas@10-20",
+            "xpDiffPerMinDeltas@10-20",
+        ]
+    ]
     means["gamesPlayed"] = playerStats[player].sum()["counter"]
-    means
     means["player"] = player
     teamSummary.append(means.to_frame().T)
 
@@ -76,20 +178,42 @@ teamSummary["KDA"] = (teamSummary["kills"] + teamSummary["assists"]) / teamSumma
     "deaths"
 ]
 
+
 # format DataFrame
 teamSummary = teamSummary.set_index("player")
-teamSummary.columns = ["winRate", "kills", "deaths", "assists", "gamesPlayed", "KDA"]
+teamSummary.columns = [
+    "winRate",
+    "kills",
+    "deaths",
+    "assists",
+    "creepsPerMin@10-20",
+    "csDiffPerMin@0-10",
+    "csDiffPerMin@10-20",
+    "xpDiffPerMin@10-20",
+    "gamesPlayed",
+    "KDA",
+]
 
 cols = teamSummary.columns.tolist()
 teamSummary = teamSummary[
-    ["gamesPlayed", "winRate", "kills", "deaths", "assists", "KDA"]
+    [
+        "gamesPlayed",
+        "winRate",
+        "kills",
+        "deaths",
+        "assists",
+        "KDA",
+        "creepsPerMin@10-20",
+        "csDiffPerMin@0-10",
+        "csDiffPerMin@10-20",
+        "xpDiffPerMin@10-20",
+    ]
 ]
 teamSummary.index.name = None
-teamSummary.sort_values("KDA", ascending="false")
-teamSummary = teamSummary.round(2)
-teamSummary
-import numpy as np
 
-np.round(teamSummary, decimals=2)
-teamSummary.to_csv("output/teamSummary.csv")
+teamSummary = teamSummary.sort_values(by="KDA", ascending=False)
+teamSummary = teamSummary.astype(float).round(2)
+teamSummary
+teamSummary.to_csv("output/team/teamSummary.csv")
+
 # for player in teamPlayers:
